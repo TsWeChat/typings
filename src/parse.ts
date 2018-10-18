4f0198a74e1a1b464b4700bfb885625c5faaf551
陈欢 <jingfeng_quietwind@163.com>
2018-10-18 09:09:01 +0800

    change

--- src/parse.ts
+++ src/parse.ts
@@ -1,24 +1,38 @@
 import { resolve } from "path"
 import * as jsdom from "jsdom"
-import * as jQuery from "jquery"
 import { crawler } from "./crawler"
+import { IChapter } from "./getApiList"
 
 const JSDOM = jsdom.JSDOM
 
-export interface IParsePaseItem {}
+export interface IParsePaseItem {
+    title: string
+    name: string
+    nameDesc: string
+    returnName: string
+    returnDesc: string
+    returnVersion?: string
+    params: string
+}
 
-export function Parse(path: string): Promise<IParsePaseItem> {
+export function Parse(path: string, chapter: IChapter): Promise<IParsePaseItem> {
     console.log(path, "start")
+
+    const { name } = chapter
 
     return crawler(path).then((res) => {
         const dom = new JSDOM(res.body)
+        const $ = require("jquery")(dom.window)
 
-        const $doc = jQuery(res.body)
+        const $goalBox = $("#book-search-results .markdown-section")
+        const title = $goalBox.find("h3").text()
+        const nameDesc = $goalBox.find("h3 + p").text()
 
-        console.log($)
+        const $return = $goalBox.find("[id='返回值'] + h5")
+        const returnName = $return.text()
+        const returnVersion = $return.next("blockquote").text()
+        const returnDesc = $return.next("p")
 
-        // console.log($.find("title").text())
-
-        return {}
+        return { name, nameDesc, title, returnName, returnVersion, returnDesc, params: "" }
     })
 }
